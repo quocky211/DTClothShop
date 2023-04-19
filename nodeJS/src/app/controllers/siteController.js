08b770447a9933bbb07d24f14c2a181c22369651
Lê Minh Đại <20521153@gm.uit.edu.vn>
2023-04-19 15:43:54 +0700

    add api

--- nodeJS/src/app/controllers/siteController.js
+++ nodeJS/src/app/controllers/siteController.js
@@ -1,6 +1,24 @@
 const Product = require('../models/products/product');
+const Category = require('../models/products/category');
+const CategoryDetail = require('../models/products/category_detail');
 
 class SiteController {
+    // GET /category
+    GetCategory(req, res, next) {
+        Category.find({})
+            .exec()
+            .then((category) => res.json(category))
+            .catch(next);
+    }
+
+    // GET /category/:id/category-detail
+    GetCategoryDetail(req, res, next) {
+        CategoryDetail.find({ category_id: req.params.id })
+            .exec()
+            .then((categoryDetail) => res.json(categoryDetail))
+            .catch(next);
+    }
+
     // GET /search
     search(req, res, next) {
         Product.paginate({ name: { $regex: req.query.name, $options: 'i' } }, { page: 1, limit: 5 })
--- nodeJS/src/routes/site.js
+++ nodeJS/src/routes/site.js
@@ -3,5 +3,7 @@ const siteController = require('../app/controllers/siteController');
 const router = express.Router();
 router.get('/search', siteController.search);
 router.get('/sort', siteController.sort);
+router.get('/category/:id/category-detail', siteController.GetCategoryDetail);
+router.get('/category', siteController.GetCategory);
 
 module.exports = router;
