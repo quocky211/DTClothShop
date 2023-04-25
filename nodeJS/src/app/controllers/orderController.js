bf90ec5a416caead5087bd39a37df33ede5b199f
Nguyễn Tuấn Kha <20521427@gm.uit.edu.vn>
2023-04-25 12:10:59 +0700

    momo

--- nodeJS/src/app/controllers/orderController.js
+++ nodeJS/src/app/controllers/orderController.js
@@ -30,6 +30,93 @@ class OrderController {
     }
     // GET /product/
     
+
+    // GET /order/checkout
+    ShowCheckOut(res, req, next){
+        var accessKey = 'w9gEg8bjA2AM2Cvr';
+        var secretKey = 'mD9QAVi4cm9N844jh5Y2tqjWaaJoGVFM';
+        var orderInfo = 'Thanh toán qua Ví MoMo';
+        var partnerCode = 'MOMO_ATM_DEV';
+        var redirectUrl = 'https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b';
+        var ipnUrl = 'https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b';
+        var requestType = "payWithMethod";
+        var amount = '10000';
+        var orderId = partnerCode + new Date().getTime();
+        var requestId = orderId;
+        var extraData ='';
+        var paymentCode = 'T8Qii53fAXyUftPV3m9ysyRhEanUs9KlOPfHgpMR0ON50U10Bh+vZdpJU7VY4z+Z2y77fJHkoDc69scwwzLuW5MzeUKTwPo3ZMaB29imm6YulqnWfTkgzqRaion+EuD7FN9wZ4aXE1+mRt0gHsU193y+yxtRgpmY7SDMU9hCKoQtYyHsfFR5FUAOAKMdw2fzQqpToei3rnaYvZuYaxolprm9+/+WIETnPUDlxCYOiw7vPeaaYQQH0BF0TxyU3zu36ODx980rJvPAgtJzH1gUrlxcSS1HQeQ9ZaVM1eOK/jl8KJm6ijOwErHGbgf/hVymUQG65rHU2MWz9U8QUjvDWA==';
+        var orderGroupId ='';
+        var autoCapture =true;
+        var lang = 'vi';
+
+        //before sign HMAC SHA256 with format
+        //accessKey=$accessKey&amount=$amount&extraData=$extraData&ipnUrl=$ipnUrl&orderId=$orderId&orderInfo=$orderInfo&partnerCode=$partnerCode&redirectUrl=$redirectUrl&requestId=$requestId&requestType=$requestType
+        var rawSignature = "accessKey=" + accessKey + "&amount=" + amount + "&extraData=" + extraData + "&ipnUrl=" + ipnUrl + "&orderId=" + orderId + "&orderInfo=" + orderInfo + "&partnerCode=" + partnerCode + "&redirectUrl=" + redirectUrl + "&requestId=" + requestId + "&requestType=" + requestType;
+        //puts raw signature
+        console.log("--------------------RAW SIGNATURE----------------")
+        console.log(rawSignature)
+        //signature
+        const crypto = require('crypto');
+        var signature = crypto.createHmac('sha256', secretKey)
+            .update(rawSignature)
+            .digest('hex');
+        console.log("--------------------SIGNATURE----------------")
+        console.log(signature)
+
+        //json object send to MoMo endpoint
+        const requestBody = JSON.stringify({
+            partnerCode : partnerCode,
+            partnerName : "Test",
+            storeId : "MomoTestStore",
+            requestId : requestId,
+            amount : amount,
+            orderId : orderId,
+            orderInfo : orderInfo,
+            redirectUrl : redirectUrl,
+            ipnUrl : ipnUrl,
+            lang : lang,
+            requestType: requestType,
+            autoCapture: autoCapture,
+            extraData : extraData,
+            orderGroupId: orderGroupId,
+            signature : signature
+        });
+        //Create the HTTPS objects
+        const https = require('https');
+        const options = {
+            hostname: 'test-payment.momo.vn',
+            port: 443,
+            path: '/v2/gateway/api/create',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(requestBody)
+            }
+        }
+        //Send the request and get the response
+        const req = https.request(options, res => {
+            console.log(`Status: ${res.statusCode}`);
+            console.log(`Headers: ${JSON.stringify(res.headers)}`);
+            res.setEncoding('utf8');
+            res.on('data', (body) => {
+                console.log('Body: ');
+                console.log(body);
+                console.log('resultCode: ');
+                console.log(JSON.parse(body).resultCode);
+            });
+            res.on('end', () => {
+                console.log('No more data in response.');
+            });
+        })
+
+        req.on('error', (e) => {
+            console.log(`problem with request: ${e.message}`);
+        });
+        // write data to request body
+        console.log("Sending....")
+        req.write(requestBody);
+        req.end();
+    }
 }
 //đoạn code sắp xếp theo giá - sẽ chèn vào giao diện
 // function sortProducts(sortOrder) {
