bbcd22fe3c3aeb625a43e6a44a5dc973432843ba
Nguyễn Tuấn Kha <20521427@gm.uit.edu.vn>
2023-04-26 10:11:41 +0700

    code has done but it has bugs and I don't know how to fix ^^

--- nodeJS/src/app/controllers/orderController.js
+++ nodeJS/src/app/controllers/orderController.js
@@ -3,6 +3,22 @@ const OrderDetail = require('../models/order/order_detail');
 const Discount = require('../models/order/discount');
 const { multipleMongooseToObject } = require('../../util/mongoose');
 const https = require('https');
+function sortObject(obj) {
+	let sorted = {};
+	let str = [];
+	let key;
+	for (key in obj){
+		if (obj.hasOwnProperty(key)) {
+		str.push(encodeURIComponent(key));
+		}
+	}
+	str.sort();
+    for (key = 0; key < str.length; key++) {
+        sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
+    }
+    return sorted;
+}
+
 class OrderController {
     // GET /product/discount
     // Discount(req, res, next) {
@@ -24,6 +40,10 @@ class OrderController {
         if (req.body.pay_method = 'momo'){
             const order = new Order(formDataOrder);
             res.redirect('/order/momocheckout', formDataOrder);
+        }
+        else if (req.body.pay_method = 'vnpay'){
+            const order = new Order(formDataOrder);
+            res.redirect('/order/vnpaycheckout', formDataOrder);
         }
         const order = new Order(formDataOrder);
         order
@@ -128,58 +148,64 @@ class OrderController {
         req.socket.remoteAddress ||
         req.connection.socket.remoteAddress;
 
-    var config = require('config');
-    
+        var config = require('config');
+        var dateFormat = require('dateformat');
 
-    
-    var tmnCode = config.get('vnp_TmnCode');
-    var secretKey = config.get('vnp_HashSecret');
-    var vnpUrl = config.get('vnp_Url');
-    var returnUrl = config.get('vnp_ReturnUrl');
 
-    var date = new Date();
+        
+        var tmnCode = 'WZ5PH4S4';
+        var secretKey = 'UYLWBXXUYFHRGCCWXAKEEQZZNPANUNAL';
+        var vnpUrl = 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html';
+        var returnUrl = "http://localhost:8888/order/vnpay_return";
+        var date = new Date();
 
-    var createDate = dateFormat(date, 'yyyymmddHHmmss');
-    var orderId = dateFormat(date, 'HHmmss');
-    var amount = req.body.amount;
-    var bankCode = req.body.bankCode;
-    
-    var orderInfo = req.body.orderDescription;
-    var orderType = req.body.orderType;
-    var locale = req.body.language;
-    if(locale === null || locale === ''){
-        locale = 'vn';
-    }
-    var currCode = 'VND';
-    var vnp_Params = {};
-    vnp_Params['vnp_Version'] = '2.1.0';
-    vnp_Params['vnp_Command'] = 'pay';
-    vnp_Params['vnp_TmnCode'] = tmnCode;
-    // vnp_Params['vnp_Merchant'] = ''
-    vnp_Params['vnp_Locale'] = locale;
-    vnp_Params['vnp_CurrCode'] = currCode;
-    vnp_Params['vnp_TxnRef'] = orderId;
-    vnp_Params['vnp_OrderInfo'] = orderInfo;
-    vnp_Params['vnp_OrderType'] = orderType;
-    vnp_Params['vnp_Amount'] = amount * 100;
-    vnp_Params['vnp_ReturnUrl'] = returnUrl;
-    vnp_Params['vnp_IpAddr'] = ipAddr;
-    vnp_Params['vnp_CreateDate'] = createDate;
-    if(bankCode !== null && bankCode !== ''){
-        vnp_Params['vnp_BankCode'] = bankCode;
-    }
+        var createDate = dateFormat(date, 'yyyymmddHHmmss');
+        var orderId = dateFormat(date, 'HHmmss');
+        // var amount = req.body.amount;
+        // var bankCode = req.body.bankCode;
+        
+        // var orderInfo = req.body.orderDescription;
+        // var orderType = req.body.orderType;
+        var amount = 1000;
+        var bankCode = 'NCB';
+        
+        var orderInfo = 'req.body.orderDescription';
+        var orderType = 'req.body.orderType';
 
-    vnp_Params = sortObject(vnp_Params);
+        var locale = req.body.language;
+        if(locale === null || locale === ''){
+            locale = 'vn';
+        }
+        var currCode = 'VND';
+        var vnp_Params = {};
+        vnp_Params['vnp_Version'] = '2.1.0';
+        vnp_Params['vnp_Command'] = 'pay';
+        vnp_Params['vnp_TmnCode'] = tmnCode;
+        // vnp_Params['vnp_Merchant'] = ''
+        vnp_Params['vnp_Locale'] = locale;
+        vnp_Params['vnp_CurrCode'] = currCode;
+        vnp_Params['vnp_TxnRef'] = orderId;
+        vnp_Params['vnp_OrderInfo'] = orderInfo;
+        vnp_Params['vnp_OrderType'] = orderType;
+        vnp_Params['vnp_Amount'] = amount * 100;
+        vnp_Params['vnp_ReturnUrl'] = returnUrl;
+        vnp_Params['vnp_IpAddr'] = ipAddr;
+        vnp_Params['vnp_CreateDate'] = createDate;
+        if(bankCode !== null && bankCode !== ''){
+            vnp_Params['vnp_BankCode'] = bankCode;
+        }
 
-    var querystring = require('qs');
-    var signData = querystring.stringify(vnp_Params, { encode: false });
-    var crypto = require("crypto");     
-    var hmac = crypto.createHmac("sha512", secretKey);
-    var signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex"); 
-    vnp_Params['vnp_SecureHash'] = signed;
-    vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
+        vnp_Params = sortObject(vnp_Params);
 
-    res.redirect(vnpUrl);
+        var querystring = require('qs');
+        var signData = querystring.stringify(vnp_Params, { encode: false });
+        var crypto = require("crypto");     
+        var hmac = crypto.createHmac("sha512", secretKey);
+        var signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex"); 
+        vnp_Params['vnp_SecureHash'] = signed;
+        vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
+
+        res.redirect(vnpUrl);
     }
 }
 //đoạn code sắp xếp theo giá - sẽ chèn vào giao diện
