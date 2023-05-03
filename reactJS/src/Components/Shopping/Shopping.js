c2174e1691d88c796b0c5f0dc733ebd0c93a01a4
quangglammm <20521514@gm.uit.edu.vn>
2023-05-03 23:31:19 +0700

    format file

--- reactJS/src/Components/Shopping/Shopping.js
+++ reactJS/src/Components/Shopping/Shopping.js
@@ -6,149 +6,188 @@ import { connect } from "react-redux";
 import { IncreaseQuantity, DecreaseQuantity, DeleteCart } from "../../actions";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert, { AlertProps } from "@mui/material/Alert";
-import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import Breadcrumb from "react-bootstrap/Breadcrumb";
 import Header from "../HeaderFolder/Header";
 import Footer from "../FooterFolder/Footer";
 const Alert = React.forwardRef(function Alert(props, ref) {
-  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
 function Shopping({ items, IncreaseQuantity, DecreaseQuantity, DeleteCart }) {
-  let ListCart = [];
-  let TotalCart = 0;
+    let ListCart = [];
+    let TotalCart = 0;
 
-  Object.keys(items.Carts).forEach(function (item) {
-    TotalCart += items.Carts[item].quantity * items.Carts[item].price;
-    ListCart.push(items.Carts[item]);
-  });
-  function TotalPrice(price, tonggia) {
-    return Number(price * tonggia).toLocaleString("vi-VN");
-  }
-  const [open, setOpen] = React.useState(false);
-
-  const handleClick = () => {
-    setOpen(true);
-  };
-
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
+    Object.keys(items.Carts).forEach(function (item) {
+        TotalCart += items.Carts[item].quantity * items.Carts[item].price;
+        ListCart.push(items.Carts[item]);
+    });
+    function TotalPrice(price, tonggia) {
+        return Number(price * tonggia).toLocaleString("vi-VN");
     }
+    const [open, setOpen] = React.useState(false);
 
-    setOpen(false);
-  };
-  return (
-    <div className="main">
-      <Header/>
-      <Breadcrumb>
-        <Breadcrumb.Item href="/">Trang chủ</Breadcrumb.Item>
-        <Breadcrumb.Item active>Giỏ hàng</Breadcrumb.Item>
-      </Breadcrumb>
-      <div className="main-container">
-        <div className="products">
-          <div className="row">
-            <div className="col-md-12">
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th></th>
-                    <th>Tên sản phẩm</th>
-                    <th>Hình</th>
-                    <th>Giá</th>
-                    <th>Số lượng</th>
-                    <th>Tổng tiền</th>
-                  </tr>
-                </thead>
-                <tbody style={{ lineHeight: "85px" }}>
-                  {ListCart.map((item, key) => {
-                    return (
-                      <tr key={key}>
-                        <td>
-                          <i
-                            className="badge badge-danger"
-                            onClick={() => {
-                              DeleteCart(key);
-                              handleClick();
-                            }}
-                          >
-                            X
-                          </i>
-                        </td>
+    const handleClick = () => {
+        setOpen(true);
+    };
 
-                        <td>{item.name}</td>
-                        <td>
-                          <img
-                            src={item.image}
-                            alt=""
-                            style={{ width: "100px", height: "80px" }}
-                          />
-                        </td>
-                        <td>
-                          {Number(item.price).toLocaleString("vi-VN")}{" "}
-                          <span className="underline">đ</span>
-                        </td>
-                        <td>
-                          <span
-                            className="btn btn-primary"
-                            style={{ margin: "2px" }}
-                            onClick={() => DecreaseQuantity(key)}
-                          >
-                            -
-                          </span>
-                          <span className="btn btn-info">{item.quantity}</span>
-                          <span
-                            className="btn btn-primary"
-                            style={{ margin: "2px" }}
-                            onClick={() => IncreaseQuantity(key)}
-                          >
-                            +
-                          </span>
-                        </td>
-                        <td>
-                          {TotalPrice(item.price, item.quantity)}{" "}
-                          <span className="underline">đ</span>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                  <tr>
-                    <td colSpan="5">Tổng tiền giỏ hàng</td>
-                    <td>
-                      {Number(TotalCart).toLocaleString("vi-VN")}{" "}
-                      <span className="underline">đ</span>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              <NavLink to="/ShipAddress" state={{ data: TotalCart }}>
-                <button className="shopping-buy-btn">Mua hàng</button>
-              </NavLink>
+    const handleClose = (event, reason) => {
+        if (reason === "clickaway") {
+            return;
+        }
+
+        setOpen(false);
+    };
+    return (
+        <div className="main">
+            <Header />
+            <Breadcrumb>
+                <Breadcrumb.Item href="/">Trang chủ</Breadcrumb.Item>
+                <Breadcrumb.Item active>Giỏ hàng</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="main-container">
+                <div className="products">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th>Tên sản phẩm</th>
+                                        <th>Hình</th>
+                                        <th>Giá</th>
+                                        <th>Số lượng</th>
+                                        <th>Tổng tiền</th>
+                                    </tr>
+                                </thead>
+                                <tbody style={{ lineHeight: "85px" }}>
+                                    {ListCart.map((item, key) => {
+                                        return (
+                                            <tr key={key}>
+                                                <td>
+                                                    <i
+                                                        className="badge badge-danger"
+                                                        onClick={() => {
+                                                            DeleteCart(key);
+                                                            handleClick();
+                                                        }}
+                                                    >
+                                                        X
+                                                    </i>
+                                                </td>
+
+                                                <td>{item.name}</td>
+                                                <td>
+                                                    <img
+                                                        src={item.image}
+                                                        alt=""
+                                                        style={{
+                                                            width: "100px",
+                                                            height: "80px",
+                                                        }}
+                                                    />
+                                                </td>
+                                                <td>
+                                                    {Number(
+                                                        item.price
+                                                    ).toLocaleString(
+                                                        "vi-VN"
+                                                    )}{" "}
+                                                    <span className="underline">
+                                                        đ
+                                                    </span>
+                                                </td>
+                                                <td>
+                                                    <span
+                                                        className="btn btn-primary"
+                                                        style={{
+                                                            margin: "2px",
+                                                        }}
+                                                        onClick={() =>
+                                                            DecreaseQuantity(
+                                                                key
+                                                            )
+                                                        }
+                                                    >
+                                                        -
+                                                    </span>
+                                                    <span className="btn btn-info">
+                                                        {item.quantity}
+                                                    </span>
+                                                    <span
+                                                        className="btn btn-primary"
+                                                        style={{
+                                                            margin: "2px",
+                                                        }}
+                                                        onClick={() =>
+                                                            IncreaseQuantity(
+                                                                key
+                                                            )
+                                                        }
+                                                    >
+                                                        +
+                                                    </span>
+                                                </td>
+                                                <td>
+                                                    {TotalPrice(
+                                                        item.price,
+                                                        item.quantity
+                                                    )}{" "}
+                                                    <span className="underline">
+                                                        đ
+                                                    </span>
+                                                </td>
+                                            </tr>
+                                        );
+                                    })}
+                                    <tr>
+                                        <td colSpan="5">Tổng tiền giỏ hàng</td>
+                                        <td>
+                                            {Number(TotalCart).toLocaleString(
+                                                "vi-VN"
+                                            )}{" "}
+                                            <span className="underline">đ</span>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <NavLink
+                                to="/ShipAddress"
+                                state={{ data: TotalCart }}
+                            >
+                                <button className="shopping-buy-btn">
+                                    Mua hàng
+                                </button>
+                            </NavLink>
+                        </div>
+                    </div>
+                    <Snackbar
+                        open={open}
+                        autoHideDuration={1500}
+                        onClose={handleClose}
+                    >
+                        <Alert
+                            onClose={handleClose}
+                            severity="error"
+                            sx={{ width: "100%" }}
+                        >
+                            Đã bỏ sản phẩm ra khỏi giỏ hàng!
+                        </Alert>
+                    </Snackbar>
+                </div>
             </div>
-          </div>
-          <Snackbar open={open} autoHideDuration={1500} onClose={handleClose}>
-            <Alert
-              onClose={handleClose}
-              severity="error"
-              sx={{ width: "100%" }}
-            >
-              Đã bỏ sản phẩm ra khỏi giỏ hàng!
-            </Alert>
-          </Snackbar>
+            <Footer />
         </div>
-      </div>
-      <Footer/>
-    </div>
-  );
+    );
 }
 const mapStateToProps = (state) => {
-  //  console.log(state)
-  return {
-    items: state._todoProduct,
-  };
+    //  console.log(state)
+    return {
+        items: state._todoProduct,
+    };
 };
 
 export default connect(mapStateToProps, {
-  IncreaseQuantity,
-  DecreaseQuantity,
-  DeleteCart,
+    IncreaseQuantity,
+    DecreaseQuantity,
+    DeleteCart,
 })(Shopping);
