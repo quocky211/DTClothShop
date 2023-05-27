6f43254e914cc379843cfe9852787c92c87ff0b4
quangglammm <20521514@gm.uit.edu.vn>
2023-05-27 22:13:33 +0700

    fix linh tinh by quanglam

--- reactJS/src/Components/Shopping/Shopping.js
+++ reactJS/src/Components/Shopping/Shopping.js
@@ -7,8 +7,7 @@ import Breadcrumb from "react-bootstrap/Breadcrumb";
 import Header from "../HeaderFolder/Header";
 import Footer from "../FooterFolder/Footer";
 import { useDispatch, useSelector } from "react-redux";
-// import CartItem from "../CartItem/CartItem";
-import {DeleteOutline} from "@mui/icons-material";
+import { DeleteOutline } from "@mui/icons-material";
 
 import {
     decreaseCart,
@@ -17,7 +16,7 @@ import {
     removefromCart,
     clearCart,
 } from "../../redux/cartSlide";
-import { red } from "@mui/material/colors";
+
 
 const Alert = React.forwardRef(function Alert(props, ref) {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
@@ -73,123 +72,6 @@ function Shopping() {
             </Breadcrumb>
             <div className="main-container">
                 <div className="products">
-                    {/* <div className="row">
-                        <div className="col-md-12">
-                            <table className="table">
-                                <thead>
-                                    <tr>
-                                        <th></th>
-                                        <th>Tên sản phẩm</th>
-                                        <th>Hình</th>
-                                        <th>Giá</th>
-
-                                        <th>Số lượng</th>
-                                        <th>Tổng tiền</th>
-                                    </tr>
-                                </thead>
-                                <tbody style={{ lineHeight: "85px" }}>
-                                    {cart.cartItems?.map((item) => {
-                                        return (
-                                            <tr key={item._id}>
-                                                <td>
-                                                    <i
-                                                        className="badge badge-danger"
-                                                        onClick={() => {
-                                                            handleRemoveFromCart(
-                                                                item
-                                                            );
-                                                            handleClick();
-                                                        }}
-                                                    >
-                                                        X
-                                                    </i>
-                                                </td>
-
-                                                <td>{item.name}<br/>{item.size},{item.color}</td>
-                                                <td>
-                                                    <img
-                                                        src={"../imgs/" + item.image}
-                                                        alt="img"
-                                                        style={{
-                                                            width: "100px",
-                                                            height: "80px",
-                                                        }}
-                                                    />
-                                                </td>
-                                                <td>
-                                                    {Number(
-                                                        item.price
-                                                    ).toLocaleString(
-                                                        "vi-VN"
-                                                    )}{" "}
-                                                    <span className="underline">
-                                                        đ
-                                                    </span>
-                                                </td>
-
-                                                <td>
-                                                    <span
-                                                        className="btn btn-primary"
-                                                        style={{
-                                                            margin: "2px",
-                                                        }}
-                                                        onClick={() =>
-                                                            handleDecreaseCart(
-                                                                item
-                                                            )
-                                                        }
-                                                    >
-                                                        -
-                                                    </span>
-                                                    <span className="btn btn-info">
-                                                        {item.quantity}
-                                                    </span>
-                                                    <span
-                                                        className="btn btn-primary"
-                                                        style={{
-                                                            margin: "2px",
-                                                        }}
-                                                        onClick={() =>
-                                                            handleIncreaseCart(
-                                                                item
-                                                            )
-                                                        }
-                                                    >
-                                                        +
-                                                    </span>
-                                                </td>
-                                                <td>
-                                                    {TotalPrice(item.quantity, item.price )}{" "}
-                                                    <span className="underline">
-                                                        đ
-                                                    </span>
-                                                </td>
-                                            </tr>
-                                        );
-                                    })}
-                                    <tr>
-                                        <td colSpan="5">Tổng tiền giỏ hàng</td>
-                                        <td>
-                                            {Number(
-                                                cart.cartTotalAmount
-                                            ).toLocaleString("vi-VN")}{" "}
-                                            <span className="underline">đ</span>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                            <button className="shopping-clear-btn" onClick={()=> handleClearCart()}>Xóa giỏ hàng</button>
-                            <NavLink
-                                to="/ShipAddress"
-                                state={{ data: cart.cartTotalAmount }}
-                            >
-                                <button className="shopping-buy-btn">
-                                    Mua hàng
-                                </button>
-                            </NavLink>
-
-                        </div>
-                    </div> */}
                     <div className="cart-heading grid grid-five-column">
                         <p>Sản phẩm</p>
                         <p className="cart-hide">Giá</p>
@@ -205,27 +87,26 @@ function Shopping() {
                                     <div className="cart-image-name">
                                         <div>
                                             <img
-                                                src={
-                                                    "../imgs/" +
-                                                    item.image
-                                                }
+                                                src={"../imgs/" + item.image}
                                                 alt={item.name}
-                                                style={
-                                                    {
-                                                        // maxWidth:125,
-                                                        // maxHeight:100,
-                                                        width: 125,
-                                                        height: 100,
-                                                    }
-                                                }
+                                                style={{
+                                                    width: 125,
+                                                    height: 100,
+                                                }}
                                             ></img>
                                         </div>
                                         <div>
                                             <p>{item.name}</p>
                                             <div className="color-div">
                                                 <p>Color:</p>
-                                                <div className="color-style"
-                                                style={{backgroundColor: item.color, color: item.color }}></div>
+                                                <div
+                                                    className="color-style"
+                                                    style={{
+                                                        backgroundColor:
+                                                            item.color,
+                                                        color: item.color,
+                                                    }}
+                                                ></div>
                                                 <span>,</span>
                                                 <p>{item.size}</p>
                                             </div>
@@ -266,28 +147,42 @@ function Shopping() {
                                         <span>đ</span>
                                     </div>
                                     <div>
-                                        <DeleteOutline className="delete-btn" onClick={()=> handleRemoveFromCart(item)}></DeleteOutline>
+                                        <DeleteOutline
+                                            className="delete-btn"
+                                            onClick={() =>
+                                                handleRemoveFromCart(item)
+                                            }
+                                        ></DeleteOutline>
                                     </div>
-
                                 </div>
                             );
                         })}
                     </div>
-                    <hr/>
+                    <hr />
                     <div className="cart-two-button">
-                        <button className="btn-clear" onClick={() => {handleClearCart()}}>Xóa giỏ hàng</button>
-                    <NavLink
-                                to="/ShipAddress"
-                                state={{ data: cart.cartTotalAmount }}
-                            >
-                                <button className="btn-pay">
-                                    {"Mua hàng"}<br/>{"("}{Number(
-                                                cart.cartTotalAmount
-                                            ).toLocaleString("vi-VN")}{" "}
-                                            <span>đ</span>{")"}
-                                </button>
-                            </NavLink>
-
+                        <button
+                            className="btn-clear"
+                            onClick={() => {
+                                handleClearCart();
+                            }}
+                        >
+                            Xóa giỏ hàng
+                        </button>
+                        <NavLink
+                            to="/ShipAddress"
+                            state={{ data: cart.cartTotalAmount }}
+                        >
+                            <button className="btn-pay">
+                                {"Mua hàng"}
+                                <br />
+                                {"("}
+                                {Number(cart.cartTotalAmount).toLocaleString(
+                                    "vi-VN"
+                                )}{" "}
+                                <span>đ</span>
+                                {")"}
+                            </button>
+                        </NavLink>
                     </div>
 
                     <Snackbar
