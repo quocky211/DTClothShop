c170c07df5ac553dbb78d1b5ae95c70b28b2484a
Nguyễn Tuấn Kha <20521427@gm.uit.edu.vn>
2023-06-06 16:42:53 +0700

    push

--- nodeJS/src/app/controllers/orderController.js
+++ nodeJS/src/app/controllers/orderController.js
@@ -38,75 +38,12 @@ class OrderController {
     ShowDiscount(req, res, next) {
         Discount.find({})
             .exec()
-            .then((discount) => res.json(product))
+            .then((discount) => res.json(discount))
             .catch(next)
     }
     // GET /order/checkout
     
-    ShowVNPayCheckOut(req, res, next) {
-        var ipAddr =
-            req.headers['x-forwarded-for'] ||
-            req.connection.remoteAddress ||
-            req.socket.remoteAddress ||
-            req.connection.socket.remoteAddress;
-
-        var config = require('config');
-        var dateFormat = require('dateformat');
-
-        var tmnCode = 'WZ5PH4S4';
-        var secretKey = 'UYLWBXXUYFHRGCCWXAKEEQZZNPANUNAL';
-        var vnpUrl = 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html';
-        var returnUrl = 'http://localhost:8888/order/vnpay_return';
-        var date = new Date();
-
-        var createDate = dateFormat(date, 'yyyymmddHHmmss');
-        var orderId = dateFormat(date, 'HHmmss');
-        // var amount = req.body.amount;
-        // var bankCode = req.body.bankCode;
-
-        // var orderInfo = req.body.orderDescription;
-        // var orderType = req.body.orderType;
-        var amount = 1000;
-        var bankCode = 'NCB';
-
-        var orderInfo = 'req.body.orderDescription';
-        var orderType = 'req.body.orderType';
-
-        var locale = req.body.language;
-        if (locale === null || locale === '') {
-            locale = 'vn';
-        }
-        var currCode = 'VND';
-        var vnp_Params = {};
-        vnp_Params['vnp_Version'] = '2.1.0';
-        vnp_Params['vnp_Command'] = 'pay';
-        vnp_Params['vnp_TmnCode'] = tmnCode;
-        // vnp_Params['vnp_Merchant'] = ''
-        vnp_Params['vnp_Locale'] = locale;
-        vnp_Params['vnp_CurrCode'] = currCode;
-        vnp_Params['vnp_TxnRef'] = orderId;
-        vnp_Params['vnp_OrderInfo'] = orderInfo;
-        vnp_Params['vnp_OrderType'] = orderType;
-        vnp_Params['vnp_Amount'] = amount * 100;
-        vnp_Params['vnp_ReturnUrl'] = returnUrl;
-        vnp_Params['vnp_IpAddr'] = ipAddr;
-        vnp_Params['vnp_CreateDate'] = createDate;
-        if (bankCode !== null && bankCode !== '') {
-            vnp_Params['vnp_BankCode'] = bankCode;
-        }
-
-        vnp_Params = sortObject(vnp_Params);
-
-        var querystring = require('qs');
-        var signData = querystring.stringify(vnp_Params, { encode: false });
-        var crypto = require('crypto');
-        var hmac = crypto.createHmac('sha512', secretKey);
-        var signed = hmac.update(new Buffer(signData, 'utf-8')).digest('hex');
-        vnp_Params['vnp_SecureHash'] = signed;
-        vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
-
-        res.redirect(vnpUrl);
-    }
+    
 
     // GET /order/:id/order-detail
     ShowOderDetailByOrder(req, res, next) {
